b94acc8a77e15391b1cb5c0bb0297586017f99e3
Miguel Oliveira <1211281@isep.ipp.pt>
2024-04-02 11:08:44 +0100

    test add to verify if javaCoCo is working

--- jobs4u.app.backoffice.console/src/test/java/jobs4u/app/backoffice/console/MainTest.java
+++ jobs4u.app.backoffice.console/src/test/java/jobs4u/app/backoffice/console/MainTest.java
@@ -1,4 +1,12 @@
 package jobs4u.app.backoffice.console;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class MainTest {
+    @Test
+    public void trueTest() {
+        assertTrue(true);
+    }
 }
