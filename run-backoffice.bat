c2fc57c185d45fbd96bf68b367c9a03d0465335c
Rodrigo <1221083@isep.ipp.pt>
2024-04-11 16:44:53 +0100

    Backoffice and bootstrap running scripts changed

--- run-backoffice.bat
+++ run-backoffice.bat
@@ -4,4 +4,4 @@ REM assumes the build was executed with maven copy-dependencies
 set JOBS4U_CP=jobs4u.app.backoffice.console\target\jobs4u.app.backoffice.console-0.1.0.jar;jobs4u.app.backoffice.console\target\dependency\*;
 
 REM call the java VM, e.g,
-java -cp %JOBS4U_CP% jobs4u.app.backoffice.console.Main
+java -cp %JOBS4U_CP% jobs4u.base.app.backoffice.console.BaseBackoffice
