96433f1e4caf3d4e3a50421ab4ce5aeaae7a8beb
MarioJoaoGR <1221019@isep.ipp.pt>
2024-04-17 23:57:44 +0100

    some work done #11

--- jobs4u.core/src/main/java/jobs4u/base/jobOpeningsManagement/application/RegisterJobOpeningController.java
+++ jobs4u.core/src/main/java/jobs4u/base/jobOpeningsManagement/application/RegisterJobOpeningController.java
@@ -1,31 +1,31 @@
-package jobs4u.base.jobOpeningsManagement.application;
-
-
-import jobs4u.base.jobOpeningsManagement.application.repositories.JobOpeningRepository;
-import jobs4u.base.jobOpeningsManagement.domain.JobReferenceService;
-
-public class RegisterJobOpeningController {
-
-    private final JobOpeningRepository jobOpeningRepository;
-    private final JobReferenceService jobReferenceService;
-
-    public RegisterJobOpeningController(JobOpeningRepository jobOpeningRepository, JobReferenceService jobReferenceService) {
-        this.jobOpeningRepository = jobOpeningRepository;
-        this.jobReferenceService = jobReferenceService;
-    }
-
+//package jobs4u.base.jobOpeningsManagement.application;
+//
+//
+//import jobs4u.base.jobOpeningsManagement.application.repositories.JobOpeningRepository;
+//import jobs4u.base.jobOpeningsManagement.domain.JobReferenceService;
+//
+//public class RegisterJobOpeningController {
+//
+//    private final JobOpeningRepository jobOpeningRepository;
+//    private final JobReferenceService jobReferenceService;
+//
+//    public RegisterJobOpeningController(JobOpeningRepository jobOpeningRepository, JobReferenceService jobReferenceService) {
+//        this.jobOpeningRepository = jobOpeningRepository;
+//       this.jobReferenceService = jobReferenceService;
+//   }
+//
 //    public JobOpening registerJobOpening(JobReference jobReference, WorkingMode workingMode, NrVacancy nrVacancy, PostalAddress address, Designation description, Designation function) {
 //
-//        final JobOpening jobOpening = new JobOpening(jobReference, workingMode, nrVacancy, address, description, function);
+//       final JobOpening jobOpening = new JobOpening(jobReference, workingMode, nrVacancy, address, description, function);
 //
-//        return saveJobOpening(jobOpening, jobReference, workingMode, nrVacancy, address, description, function);
+//       return saveJobOpening(jobOpening, jobReference, workingMode, nrVacancy, address, description, function);
 //    }
 //
-//    private saveJobOpening(JobOpening jobOpening, JobReference jobReference, WorkingMode workingMode, NrVacancy nrVacancy, PostalAddress address, Designation description, Designation function){
+//   private saveJobOpening(JobOpening jobOpening, JobReference jobReference, WorkingMode workingMode, NrVacancy nrVacancy, PostalAddress address, Designation description, Designation function){
 //
-//        jobOpening = this.jobOpeningRepository.save(jobOpening);
+//       jobOpening = this.jobOpeningRepository.save(jobOpening);
 //
-//    }
-
-
-}
+//  }
+//
+//
+//}
