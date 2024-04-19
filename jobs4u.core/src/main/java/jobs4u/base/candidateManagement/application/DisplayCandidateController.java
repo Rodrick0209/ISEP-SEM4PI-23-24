a68029d4049eb627a72e8dcea3a99d7c5b25a5c7
David Ferreira da Silva Marques <1221276@isep.ipp.pt>
2024-04-19 12:11:54 +0100

    metodo para listar todos os candidatos #14

--- jobs4u.core/src/main/java/jobs4u/base/candidateManagement/application/DisplayCandidateController.java
+++ jobs4u.core/src/main/java/jobs4u/base/candidateManagement/application/DisplayCandidateController.java
@@ -1,7 +1,28 @@
 package jobs4u.base.candidateManagement.application;
 
+import jobs4u.base.candidateManagement.application.repositories.CandidateRepository;
+import jobs4u.base.infrastructure.persistence.PersistenceContext;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class DisplayCandidateController {
 
+    private final CandidateMapper candidateMapper = new CandidateMapper();
+
+    private final CandidateRepository candidateRepository = PersistenceContext.repositories().candidates();
+
+    private List<CandidateDTO> candidatesFromRepository() {
+
+        ArrayList<CandidateDTO> candidates = new ArrayList<>();
+
+        candidateRepository.findAll().forEach(candidate -> {
+            candidates.add(candidateMapper.toDTO(candidate));
+        });
+
+        return candidates;
+
+    }
 
 
 }
