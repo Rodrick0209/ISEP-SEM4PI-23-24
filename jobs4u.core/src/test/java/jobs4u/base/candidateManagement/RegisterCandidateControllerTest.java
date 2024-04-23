ca1006eafdf6eee17ee09a080b89571849639c3b
David Ferreira da Silva Marques <1221276@isep.ipp.pt>
2024-04-23 16:20:29 +0100

    testes para 2000a feitos #13

--- jobs4u.core/src/test/java/jobs4u/base/candidateManagement/RegisterCandidateControllerTest.java
+++ jobs4u.core/src/test/java/jobs4u/base/candidateManagement/RegisterCandidateControllerTest.java
@@ -12,6 +12,9 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 
 import org.mockito.ArgumentMatchers;
+import org.springframework.dao.DataIntegrityViolationException;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 
@@ -47,16 +50,18 @@ public class RegisterCandidateControllerTest {
     @Test
     void ensureCandidateDoesRegisterWithExistingEmail() {
 
-        // TODO - FIND HOW TO THE EMAIL ID'ING WORKS
 
         final Candidate candidate = new Candidate("nome", "apelido", "email@email.pt", "965430293");
         final Candidate candidate2 = new Candidate("nomedois", "apelidodois", "email@email.pt", "965430293");
 
         when(candidateRepository.save(candidate)).thenReturn(candidate);
-        when(candidateRepository.save(candidate2)).thenReturn(candidate2);
 
-        final var reg1 = subject.registerCandidate("email@email.pt", "nome", "apelido", "965430293");
-        final var reg2 = subject.registerCandidate("email@email.pt", "nomedois", "apelidodois", "965430293");
+        subject.registerCandidate("email@email.pt", "nome", "apelido", "965430293");
 
+        when(candidateRepository.save(candidate2)).thenThrow(new DataIntegrityViolationException(""));
+
+        assertThrows(DataIntegrityViolationException.class, () ->
+                subject.registerCandidate("email@email.pt", "nomedois", "apelidodois", "965430293")
+        );
     }
 }
