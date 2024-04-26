ef2813965b358571d5ee8a939782767ac71e476e
rodri <1220636@isep.ipp.pt>
2024-04-26 11:52:50 +0100

    fixing location Us1012  #21

--- jobs4u.app.backoffice.console/src/main/java/jobs4u/base/app/backoffice/console/presentation/costumerManagerUser/MainTest.java
+++ jobs4u.app.backoffice.console/src/main/java/jobs4u/base/app/backoffice/console/presentation/costumerManagerUser/MainTest.java
@@ -1,4 +1,4 @@
-package jobs4u.base.app.backoffice.console.presentation.customerManagerUser;
+package jobs4u.base.app.backoffice.console.presentation.costumerManagerUser;
 
 public class MainTest {
 
