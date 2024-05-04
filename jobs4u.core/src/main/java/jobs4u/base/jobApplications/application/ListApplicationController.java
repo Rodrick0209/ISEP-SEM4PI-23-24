a0c5b8712d075f0347bea910658320764c773c94
Miguel Oliveira <1211281@isep.ipp.pt>
2024-05-05 00:02:56 +0100

    fix error in ListApplicationController #17

--- jobs4u.core/src/main/java/jobs4u/base/jobApplications/application/ListApplicationController.java
+++ jobs4u.core/src/main/java/jobs4u/base/jobApplications/application/ListApplicationController.java
@@ -1,31 +1,31 @@
 package jobs4u.base.jobApplications.application;
 
-import jakarta.persistence.Persistence;
 import jobs4u.base.infrastructure.persistence.PersistenceContext;
 import jobs4u.base.jobApplications.domain.JobApplication;
-import jobs4u.base.jobApplications.repositories.JobApplicationRepository;
 import jobs4u.base.jobOpeningsManagement.domain.JobOpening;
 import jobs4u.base.jobOpeningsManagement.repositories.JobOpeningRepository;
 
 public class ListApplicationController {
 
     private final JobOpeningRepository repository = PersistenceContext.repositories().jobOpenings();
-
-    public Iterable<JobOpening> listJobOpenings() {
     private final JobOpeningRepository jobOpeningRepository = PersistenceContext.repositories().jobOpenings();
 
-    public Iterable<JobApplication> listApplications() {
-        return repository.findAll();
+        public Iterable<JobOpening> listJobOpenings() {
+            return jobOpeningRepository.findAll();
+        }
+
+        public Iterable<JobOpening> listApplications() {
+            return repository.findAll();
+        }
+
+        public Iterable<JobApplication> listApplications(JobOpening jobOpening) {
+            return repository.ofIdentity(jobOpening.jobReference()).get().getApplications();
+        }
+        public JobOpening getJobOpeningForJobApplication(JobApplication jobApplication) {
+            return jobOpeningRepository.findByJobApplication(jobApplication);
+        }
+
+
+
+
     }
-
-    public Iterable<JobApplication> listApplications(JobOpening jobOpening) {
-        return repository.ofIdentity(jobOpening.jobReference()).get().getApplications();
-    }
-    public JobOpening getJobOpeningForJobApplication(JobApplication jobApplication) {
-        return jobOpeningRepository.findByJobApplication(jobApplication);
-    }
-
-
-
-
-}
