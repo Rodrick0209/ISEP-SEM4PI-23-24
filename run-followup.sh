d8ed950c8e58ef3bf00aa5f3fb92ff3e59a1ac7e
Miguel Oliveira <1211281@isep.ipp.pt>
2024-05-31 15:46:01 +0100

    US3002 update in run-followup.sh #40

--- run-followup.sh
+++ run-followup.sh
@@ -1,6 +1,7 @@
-#!/usr/bin/env bash
+#!/bin/bash
+# set the class path,
+# assumes the build was executed with maven copy-dependencies
+JOBS4U_CP="jobs4u.deamon.followup/target/followup-0.1.0.jar:jobs4u.deamon.followup/target/dependency/*"
 
-export JOBS4U_CP="jobs4u/target/jobs4u.deamon.followup-0.1.0.jar:jobs4u.deamon.followup/target/dependency/*;"
-
-
-java -cp $JOBS4U_CP jobs4u.deamon.followup/src/main/java/jobs4u/base/FollowUpDeamon.java
+# call the java VM, e.g,
+java -cp $JOBS4U_CP jobs4u.server.deamon.FollowUpDeamon
