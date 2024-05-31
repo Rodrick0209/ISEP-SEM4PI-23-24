7c50ee04f85bef4ed33d027a06d3bdce9235f353
David Ferreira da Silva Marques <1221276@isep.ipp.pt>
2024-05-31 14:46:30 +0100

    update run-followup

--- run-followup.sh
+++ run-followup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export JOBS4U_CP="jobs4u.deamon.followup/target/jobs4u.deamon.followup-0.1.0.jar:jobs4u.deamon.followup/target/dependency/*;"
+export JOBS4U_CP="jobs4u/target/jobs4u.deamon.followup-0.1.0.jar:jobs4u.deamon.followup/target/dependency/*;"
 
 
-java -cp $JOBS4U_CP jobs4u.deamon.followup.FollowUpDaemon
+java -cp $JOBS4U_CP jobs4u.deamon.followup/src/main/java/jobs4u/base/FollowUpDeamon.java
