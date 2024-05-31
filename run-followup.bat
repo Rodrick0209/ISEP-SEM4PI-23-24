cba0407866f75709bff9cc9c713472297b0eb300
David Ferreira da Silva Marques <1221276@isep.ipp.pt>
2024-05-31 22:17:26 +0100

    update run-followup.bat

--- run-followup.bat
+++ run-followup.bat
@@ -1,7 +1,7 @@
 @echo off
 REM set the class path,
 REM assumes the build was executed with maven copy-dependencies
-set JOBS4U_CP=jobs4u.deamon.followup/target/jobs4u.deamon.followup-0.1.0.jar;jobs4u.deamon.followup/target/dependency/*;
+set JOBS4U_CP=jobs4u.deamon.followup\target\followup-0.1.0.jar;jobs4u.deamon.followup\target\dependency\*
 
 REM call the java VM, e.g,
-java -cp %JOBS4U_CP% jobs4u.deamon.followup/src/main/java/jobs4u/base/FollowUpDeamon.java
+java -cp %JOBS4U_CP% jobs4u.server.deamon.FollowUpDeamon
